3fd7c09d98fb23c9807931c6363ad1181eb283f1
P-FClus <168067821+P-FClus@users.noreply.github.com>
2024-04-25 22:36:00 +0530

    Synthetic Dataset Uploaded

---
+++ synthetic_dataset/data_gen_no.py
@@ -0,0 +1,50 @@
+import random
+import numpy as np
+import csv
+import array as arr
+import math
+
+def gauss_2d(mu, sigma):
+    # x cordinate of datapoint
+    x = random.gauss(mu, sigma)
+    # y cordinate of datapoit
+    y = random.gauss(mu, sigma)
+    return (x, y)
+
+# homogeneity of the dataset. That is the number of gaussians that each client gets its data from.
+homo = int(input("Enter Homogeneity: "))
+# number of data in client 1
+points_in_client1 = int(input("Enter the total data points for client 1: ")) 
+# additional data on increasing the clients
+additional_points_per_cluster = int(input("Enter the additional data points per cluster: "))
+
+for _ in range(1):
+    total_clusters = 20
+    for _ in range(1):
+        data = []
+        total_clients = 1000 # Different for different dataset
+        for j in range(total_clients):
+            curr = [] # stores newly generated datapoint
+            all_gauss = [g for g in range (total_clusters)]  
+            random.shuffle(all_gauss) # randomly selects a gaussian
+            gauss = all_gauss[:homo] # stores selected gaussians in gauss
+            for hh in range(homo):
+                i = gauss[hh]
+                total_points = points_in_client1+j*additional_points_per_cluster
+                from_ith_cluster_to_jth_client = math.ceil(total_points/homo)
+                for n in range(from_ith_cluster_to_jth_client):
+                    x_val,y_val = gauss_2d(10*i,1)
+                    if(len(curr) == total_points):
+                        break
+                    else:
+                        curr.append([x_val,y_val])
+                        data.append([x_val,y_val]) # stores the datapoint in the data
+                    
+# File name for the CSV file
+        file_name = f"k_{total_clusters}_c_{total_clients}_h_{homo}_p_{points_in_client1}_a_{additional_points_per_cluster}_nooverlap_big_data.csv"
+
+# Writing data to the CSV file
+        with open(file_name, 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(data)
+
